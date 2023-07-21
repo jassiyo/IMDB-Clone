b70fad68af2b3708835f150b7887fc3bd972dc41
Jaskirat <jaskirathsingh2@gmail.com>
2023-07-21 10:53:25 +0530

    color yellow to green

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
